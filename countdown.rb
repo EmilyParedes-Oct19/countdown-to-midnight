8dadfb2db2e3f099e061722d478e12e5172914de
Emilliana Paredes <emilliana.paredes@gmail.com>
2020-10-22 23:12:25 -0500

    commit tree 12dc584dbd0268853f750ed5485de3d48ba308b8

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
 #write your code here
 
-def countdown
+def countdown (seconds_to_midnight)
+    while seconds_to_midnight > 0 
+        puts "#{seconds_to_midnight} SECOND(S)!" 
+        seconds_to_midnight -= 1
+    end   
+    until seconds_to_midnight == 1    
+        return "HAPPY NEW YEAR!"
+    end    
+end
+
+def countdown_with_sleep (time)
+      while time > 0
+        time -= 1
+        sleep 1
+    end
 end
